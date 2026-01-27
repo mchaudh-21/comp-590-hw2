8f7854093b7783f19ea781725acd7a6bcbf9d6a7
Emma Coye <ecoye@unc.edu>
2026-01-26 20:29:51 -0500

    Adding linter to prevent deadlock

--- dining/Main.java
+++ dining/Main.java
@@ -1,4 +1,6 @@
 package dining;
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static final int NUM_PHILOSOPHERS = 5;
     public static void main(String[] args) {
@@ -8,11 +10,14 @@ public class Main {
             forks[i] = new Fork(i);
         }
 
+        // only 4 can eat at a time, so one is always eating
+        Semaphore eatingLimiter = new Semaphore(NUM_PHILOSOPHERS - 1, true);
+
         for (int i = 0; i< NUM_PHILOSOPHERS; i++) {
             Fork left = forks[i];
             Fork right = forks[(i+1) % NUM_PHILOSOPHERS];
             Thread t = new Thread(
-                    new Philosopher(i, left, right),
+                    new Philosopher(i, left, right, eatingLimiter),
                     "Philosopher " +i
             );
             t.start();
--- dining/Philosopher.java
+++ dining/Philosopher.java
@@ -1,13 +1,17 @@
 package dining;
+import java.util.concurrent.Semaphore;
+
 public class Philosopher implements Runnable {
     private final int id;
     private final Fork left;
     private final Fork right;
+    private final Semaphore eatingLimiter; //only 4 can eat at a time
 
-    public Philosopher(int id, Fork left, Fork right) {
+    public Philosopher(int id, Fork left, Fork right, Semaphore eatingLimiter) {
         this.id = id;
         this.left = left;
         this.right = right;
+        this.eatingLimiter = eatingLimiter;
     }
 
     @Override
@@ -27,32 +31,39 @@ public class Philosopher implements Runnable {
         Thread.sleep(200);
     }
     private void eat() throws InterruptedException {
-        pickUpLeftFork();
-        pickUpRightFork();
-        System.out.println("Philosopher " +id+" EATING");
-        Thread.sleep(200);
-        putDownLeftFork();
-        putDownRightFork();
+        eatingLimiter.acquire();
+        
+        try {
+            pickUpLeftFork();
+            pickUpRightFork();
+            System.out.println("Philosopher " +id+" EATING");
+            Thread.sleep(200);
+            putDownLeftFork();
+            putDownRightFork();
+        } finally {
+            // Always realease to prevent starvation of any philosopher
+            eatingLimiter.release();
+        }
     }
 
     private void pickUpLeftFork() throws InterruptedException{
         // thread will wait until the fork is free
         
-        // System.out.println("Philosopher" + id + " picked up fork " + left.getId());
+        //System.out.println("Philosopher" + id + " picked up fork " + left.getId());
         left.acquire();   
     }
 
     private void pickUpRightFork() throws InterruptedException {
-        // System.out.println("Philosopher" + id + " picked up fork " + right.getId());
+        //System.out.println("Philosopher" + id + " picked up fork " + right.getId());
         right.acquire();
     }
 
     private void putDownLeftFork() throws InterruptedException {
-        // System.out.println("Philosopher" + id + " put down fork " + left.getId());
+        //System.out.println("Philosopher" + id + " put down fork " + left.getId());
         left.release();
     }
     private void putDownRightFork() throws InterruptedException {
-        // System.out.println("Philosopher" + id + " put down fork " + right.getId());
+        //System.out.println("Philosopher" + id + " put down fork " + right.getId());
         right.release();
     }
 
